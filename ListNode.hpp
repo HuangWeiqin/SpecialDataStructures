3226ad0c1d699626daa234b373ae4a4c376b432e
HuangWeiqin <weiqin.huang96@gmail.com>
2022-06-16 14:20:42 +0800

    好了 现在listnode就是单纯的listnode了

--- ListNode.hpp
+++ ListNode.hpp
@@ -1,6 +1,5 @@
 #ifndef LISTNODE_INCLUDED
 #include <iostream>
-#include "Handler.hpp"
 using namespace std;
 
 template <typename T>
@@ -17,7 +16,7 @@ class LN_MetaData{
 };
 
 template <typename T>
-class ListNode :public Handler<T>{
+class ListNode{
     public:
         ListNode():ListNode('~'){meta_info->size = 0;};
         ListNode(const T&);
@@ -27,11 +26,14 @@ class ListNode :public Handler<T>{
         virtual ListNode<T>& operator=(const ListNode<T>&);
         void emplace(int,const T*);
         void debug_print();
-        void insert_right(const T&);
-        void insert_left(const T&);
+        ListNode<T>* insert_right(const T&);
+        ListNode<T>* insert_left(const T&);
+        void set_value(const T& dt){data = dt;}
+        const T& value() const {return data;}
     private:
         ListNode(const T&,LN_MetaData<T> *);
         ListNode(const ListNode<T>&,LN_MetaData<T> *);
+        T data;
         LN_MetaData<T> *meta_info = nullptr;
         ListNode<T> *left = nullptr;
         ListNode<T> *right = nullptr;
@@ -45,8 +47,6 @@ class ListNode :public Handler<T>{
 
 template <typename T>
 ListNode<T>& ListNode<T>::operator=(const ListNode<T>& other){
-    this->share_from(other);
-
     meta_info->size = other.meta_info->size;
     if(other.left != nullptr){
         left = other.left->copy_left(meta_info);
@@ -57,13 +57,13 @@ ListNode<T>& ListNode<T>::operator=(const ListNode<T>& other){
 }
 
 template <typename T>
-ListNode<T>::ListNode(const T &dt):Handler<T>(dt),meta_info(new LN_MetaData<T>(1,this,this)){}
+ListNode<T>::ListNode(const T &dt):data(dt),meta_info(new LN_MetaData<T>(1,this,this)){}
 
 template <typename T>
-ListNode<T>::ListNode(const T &dt, LN_MetaData<T> *meta):Handler<T>(dt),meta_info(meta){}
+ListNode<T>::ListNode(const T &dt, LN_MetaData<T> *meta):data(dt),meta_info(meta){}
 
 template <typename T>
-ListNode<T>::ListNode(const ListNode<T>& other):Handler<T>(other),meta_info(new LN_MetaData<T>(1,this,this)){
+ListNode<T>::ListNode(const ListNode<T>& other):data(other.data),meta_info(new LN_MetaData<T>(1,this,this)){
     meta_info->size = other.meta_info->size;
     if(other.left != nullptr){
         left = other.left->copy_left(meta_info);
@@ -74,7 +74,7 @@ ListNode<T>::ListNode(const ListNode<T>& other):Handler<T>(other),meta_info(new 
 }
 
 template <typename T>
-ListNode<T>::ListNode(const ListNode<T>& other, LN_MetaData<T> *meta):Handler<T>(other),meta_info(meta){}
+ListNode<T>::ListNode(const ListNode<T>& other, LN_MetaData<T> *meta):data(other.data),meta_info(meta){}
 
 template <typename T>
 void ListNode<T>::emplace(int nb, const T* dt){
@@ -82,24 +82,19 @@ void ListNode<T>::emplace(int nb, const T* dt){
         return;
     }
 
-    this->set_value(*dt);
-
-    if(--nb <= 0){
-        return;
+    if(right == nullptr){
+        insert_right(*dt)->emplace(nb-1,dt+1);
+    }
+    else{
+        right->emplace(nb-1,dt+1);
+        data = *dt;
     }
 
-    if(right == nullptr)
-        insert_right(*dt);
-
-    
-    if(right != nullptr)
-        right->emplace(nb,++dt);
-    
 }
 
 
 template <typename T>
-ListNode<T>::ListNode(int nb, const T* ptr):ListNode<T>(*ptr){
+ListNode<T>::ListNode(int nb, const T* ptr):ListNode<T>(){
     emplace(nb,ptr);
 }
 
@@ -175,17 +170,17 @@ void ListNode<T>::debug_print(){
     cout<<" size: "<<meta_info->size<<endl;
     while (cur != nullptr)
     {
-        cout<<cur->value()<<" coutn: "<<cur->share_count() <<" size: "<< cur->meta_info->size<< endl;
+        cout<<cur->value() <<" size: "<< cur->meta_info->size<< endl;
         cur = cur->right;
     }
     cout<<endl;
 }
 
 template <typename T>
-void ListNode<T>::insert_right(const T& val){
+ListNode<T>* ListNode<T>::insert_right(const T& val){
     if(meta_info->size++ == 0){
         this->set_value(val);
-        return;
+        return this;
     }
 
     ListNode<T> *newcommer = new ListNode<T>(val,meta_info);
@@ -201,13 +196,15 @@ void ListNode<T>::insert_right(const T& val){
         newcommer->left = this;
         right = newcommer;
     }
+
+    return newcommer;
 }
 
 template <typename T>
-void ListNode<T>::insert_left(const T& val){
+ListNode<T>* ListNode<T>::insert_left(const T& val){
     if(meta_info->size++ == 0){
         this->set_value(val);
-        return;
+        return this;
     }
 
     ListNode<T> *newcommer = new ListNode<T>(val,meta_info);
@@ -223,6 +220,8 @@ void ListNode<T>::insert_left(const T& val){
         newcommer->right = this;
         left = newcommer;
     }
+
+    return newcommer;
 }
 
 #define LISTNODE_INCLUDED
